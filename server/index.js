d213202e038fbc214d13e1b38d651d488e0de24b
ZxcCC <731458684@qq.com>
2019-10-05 20:34:50 +0800

    ddd

--- server/index.js
+++ server/index.js
@@ -1,28 +1,35 @@
 //创建Express应用程序
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 //使用中间件
 //内置的解析中间件，确保客户端返回的数据的可靠性
-app.use(express.json())
+app.use(express.json());
 //CORS跨域的中间件
-app.use(require('cors')())
+app.use(require("cors")());
 //这句代码需要在express.static上面
-var history = require('connect-history-api-fallback')
-app.use('/',history());
+var history = require("connect-history-api-fallback");
+app.use(
+  history({
+    verbose: true,
+    index: "/"
+  })
+);
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "web/index.html"));
+});
 //静态资源托管
-app.use('/admin', express.static(__dirname + '/admin'))
-app.use('/uploads', express.static(__dirname + '/uploads'))
-app.use('/', express.static(__dirname + '/web'))
+app.use("/admin", express.static(__dirname + "/admin"));
+app.use("/uploads", express.static(__dirname + "/uploads"));
+app.use("/", express.static(__dirname + "/web"));
 
 //设置一个密钥
-app.set('secret', 'i2u34y12oi3u4y8')
+app.set("secret", "i2u34y12oi3u4y8");
 //将实例app注入模块内
-require('./plugins/db')(app)
-require('./routes/admin')(app)
-require('./routes/web')(app)
+require("./plugins/db")(app);
+require("./routes/admin")(app);
+require("./routes/web")(app);
 
-
-
-app.listen(3000,()=>{
-    console.log('http://localhost:3000')
-})
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
